2e7e257f85c3c04e6088a8bd52ae05c7290350cb
Suheyb Coskun <suheyb.c@gmail.com>
2020-01-10 23:56:26 +0100

    feat: create MongoDB connection, ignore next.config.js, put necessary info into it

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,4 +1,7 @@
 import products from '../../public/static/products.json';
+import connectDB from '../../utils/connectDb';
+
+connectDB();
 
 export default (req, res) => {
   res.status(200).json(products);
---
+++ utils/connectDb.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const connection = {};
+
+const connectDB = async () => {
+  if (connection.isConnected) {
+    console.log('Using existing DB connection');
+    return;
+  }
+
+  const db = await mongoose.connect(process.env.MONGO_SRV, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log('DB connected');
+  connection.isConnected = db.connections[0].readyState;
+};
+
+export default connectDB;

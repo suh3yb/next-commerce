7713b02f98e9b7c10fd4232ca0f897c0e90a915f
Suheyb Coskun <suheyb.c@gmail.com>
2020-01-10 23:13:59 +0100

    create header with links and progress bar, set server, add getInitialProps into _app and index page, display products in homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
-function Home() {
-  return <>home</>;
-}
+import axios from 'axios';
+
+const Home = ({ products }) => {
+  return (
+    <>
+      {products.map(product => (
+        <div key={product.sku}>
+          <h1>{product.name}</h1>
+          <p>{product.description}</p>
+          <h3>{product.price}</h3>
+          <br />
+        </div>
+      ))}
+    </>
+  );
+};
+
+Home.getInitialProps = async () => {
+  const url = 'http://localhost:3000/api/products';
+  const response = await axios.get(url);
+  return { products: response.data };
+};
 
 export default Home;

ab7a81e681b5baa0edc9b4bb79f7f1f9277a4d04
Suheyb Coskun <suheyb.c@gmail.com>
2020-01-13 23:27:07 +0100

    add baseUrl util, add delete product functionality

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import ProductList from '../components/Index/ProductList';
+import baseUrl from '../utils/baseUrl';
 
 const Home = ({ products }) => {
   return <ProductList products={products} />;
 };
 
 Home.getInitialProps = async () => {
-  const url = 'http://localhost:3000/api/products';
+  const url = `${baseUrl}/api/products`;
   const response = await axios.get(url);
   return { products: response.data };
 };

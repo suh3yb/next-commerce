a5f3e65009777b2259cba379a6ae9713efb7b3e7
Suheyb Coskun <suheyb.c@gmail.com>
2020-01-15 22:42:28 +0100

    create cart page with its components

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,5 +1,15 @@
-function CartSummary() {
-  return <>CartSummary</>;
-}
+import { Segment, Divider, Button } from 'semantic-ui-react';
+
+const CartSummary = () => {
+  return (
+    <>
+      <Divider />
+      <Segment clearing size="large">
+        <strong>Sub total:</strong> $0.00
+        <Button icon="cart" color="teal" floated="right" content="Checkout" />
+      </Segment>
+    </>
+  );
+};
 
 export default CartSummary;

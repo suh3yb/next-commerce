f6afcc9b1c5156cbd33894e6994ebb224482abf7
Suheyb Coskun <suheyb.c@gmail.com>
2020-01-17 10:23:02 +0100

    create user model, signup api with validations, auth to provide token cookie

---
+++ pages/api/signup.js
@@ -0,0 +1,42 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import isEmail from 'validator/lib/isEmail';
+import isLength from 'validator/lib/isLength';
+import connectDB from '../../utils/connectDb';
+import User from '../../models/User';
+
+connectDB();
+
+export default async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    if (!isLength(name, { min: 3, max: 10 })) {
+      return res.status(422).send('Name must be 3-10 characters long');
+    } else if (!isLength(password, { min: 6 })) {
+      return res.status(422).send('Password must be at least 6 characters');
+    } else if (!isEmail(email)) {
+      return res.status(422).send('Email must be valid');
+    }
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(422).send(`User already exists with email ${email}`);
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+
+    const newUser = await new User({
+      name,
+      email,
+      password: hash,
+    }).save();
+
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: '7d',
+    });
+    res.status(201).json(token);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error signup user. Please try again later.');
+  }
+};

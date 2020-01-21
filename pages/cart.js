59d441ac8668e29e58484f510bebf94cd3856f2f
Suheyb Coskun <suheyb.c@gmail.com>
2020-01-21 23:18:35 +0100

    create cart api, add logic to get user cart

--- pages/cart.js
+++ pages/cart.js
@@ -1,14 +1,27 @@
 import { Segment } from 'semantic-ui-react';
+import axios from 'axios';
+import { parseCookies } from 'nookies';
 import CartItemList from '../components/Cart/CartItemList';
 import CartSummary from '../components/Cart/CartSummary';
+import baseUrl from '../utils/baseUrl';
 
-const Cart = () => {
+const Cart = ({ products }) => {
+  console.log(products);
   return (
     <Segment>
       <CartItemList />
       <CartSummary />
     </Segment>
   );
+};
+
+Cart.getInitialProps = async ctx => {
+  const { token } = parseCookies(ctx);
+  if (!token) return { products: [] };
+  const url = `${baseUrl}/api/cart`;
+  const payload = { headers: { Authorization: token } };
+  const response = await axios.get(url, payload);
+  return { products: response.data };
 };
 
 export default Cart;

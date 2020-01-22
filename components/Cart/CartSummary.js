58761dce382f784bc78edc16f42ff14192560451
Suheyb Coskun <suheyb.c@gmail.com>
2020-01-22 22:40:48 +0100

    add style for cart products, calculation for cart total

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,12 +1,31 @@
+import { useState, useEffect } from 'react';
 import { Segment, Divider, Button } from 'semantic-ui-react';
+import calculateCartTotal from '../../utils/calculateCartTotal';
 
-const CartSummary = () => {
+const CartSummary = ({ products }) => {
+  const [cartAmount, setCartAmount] = useState(0);
+  const [stripeAmount, setStripeAmount] = useState(0);
+  const [isCartEmpty, setCartEmpty] = useState(false);
+
+  useEffect(() => {
+    const { cartTotal, stripeTotal } = calculateCartTotal(products);
+    setCartAmount(cartTotal);
+    setStripeAmount(stripeTotal);
+    setCartEmpty(products.length === 0);
+  }, [products]);
+
   return (
     <>
       <Divider />
       <Segment clearing size="large">
-        <strong>Sub total:</strong> $0.00
-        <Button icon="cart" color="teal" floated="right" content="Checkout" />
+        <strong>Sub total:</strong> ${cartAmount}
+        <Button
+          icon="cart"
+          disabled={isCartEmpty}
+          color="teal"
+          floated="right"
+          content="Checkout"
+        />
       </Segment>
     </>
   );
--- pages/cart.js
+++ pages/cart.js
@@ -5,12 +5,11 @@ import CartItemList from '../components/Cart/CartItemList';
 import CartSummary from '../components/Cart/CartSummary';
 import baseUrl from '../utils/baseUrl';
 
-const Cart = ({ products }) => {
-  console.log(products);
+const Cart = ({ products, user }) => {
   return (
     <Segment>
-      <CartItemList />
-      <CartSummary />
+      <CartItemList products={products} user={user} />
+      <CartSummary products={products} />
     </Segment>
   );
 };
